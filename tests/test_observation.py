1260b275fb9e9900c922e037c6956f1b5330960e
Colin S <3526918+cbs228@users.noreply.github.com>
2018-11-17 16:40:38 -0600

    Split framing and parsing
    Split out the tasks of decoding the message from the task of
    framing the messages. Now, the ThermometerProtocol is responsible
    for performing frame detection.
    
    Data strings for unit tests are separated into their own file.

--- tests/test_observation.py
+++ tests/test_observation.py
@@ -1,33 +1,9 @@
-from datetime import timedelta
 import math
 
 from tc2100.observation import Observation, ThermocoupleType, TemperatureUnit,\
     MeterTime
 
-NO_DATA = (b"\x65\x14\x00\x00\x00\x09\xB0\x09\xB4\x07\x81"
-           b"\x40\x40\x00\x06\x2A\x0D\x0A")
-
-CH1 = (b"\x65\x14\x00\x00\x00\x00\xD0\x09\xB3\x07\x81\x08"
-       b"\x40\x00\x0B\x2B\x0D\x0A")
-
-CH2 = (b"\x65\x14\x00\x00\x00\x09\xAF\x00\xCC\x07\x81\x40"
-       b"\x08\x00\x0C\x0C\x0D\x0A")
-
-BOTH = (b"\x65\x14\x00\x00\x00\x00\xE4\x00\xE8\x01\x81"
-        b"\x08\x08\x02\x1C\x1E\x0D\x0A")
-
-BOTH_K = (b"\x65\x14\x00\x00\x00\x0B\x8E\x0B\x95\x01\x83"
-          b"\x08\x08\x00\x02\x22\x0D\x0A")
-BOTH_K_CH1 = 295.8
-BOTH_K_CH2 = 296.5
-
-FRAMING_LEADZERO = b"\x00" + BOTH_K
-
-FRAMING_BAD_HEADER = b"\x00\x65\x14" + BOTH_K
-
-FRAMING_TRAIL_HEADER = BOTH_K + b"\x65\x14\xFF"
-
-FRAMING_TWOMSGS = BOTH_K + BOTH_K
+from . import datastrings
 
 
 def test_temp_unit_strings():
@@ -49,106 +25,67 @@ def test_metertime_create():
 
 
 def test_metertime_packunpack():
-    m = MeterTime.from_bytes(NO_DATA[13:16])
+    m = MeterTime.from_bytes(datastrings.NO_DATA[13:16])
     assert m.hours == 0
     assert m.seconds == 42
     assert m.minutes == 6
 
 
 def test_decode_no_data():
-    msg = Observation.from_bytes(NO_DATA)
+    msg = Observation.from_bytes(datastrings.NO_DATA[5:5+11])
     assert msg.thermocouple_type == ThermocoupleType.N
-    assert msg.channel_temp[0] != msg.channel_temp[0]
-    assert msg.channel_temp[1] != msg.channel_temp[1]
-    assert msg.units == TemperatureUnit.C
-    assert msg.meter_time == timedelta(seconds=42, minutes=6)
+    assert msg.temperatures[0] != msg.temperatures[0]
+    assert msg.temperatures[1] != msg.temperatures[1]
+    assert msg.unit == TemperatureUnit.C
+    assert str(msg.meter_time) == "000:06:42"
 
 
 def test_decode_ch1():
-    msg = Observation.from_bytes(CH1)
+    msg = Observation.from_bytes(datastrings.CH1[5:5+11])
     assert msg.thermocouple_type == ThermocoupleType.N
-    assert msg.channel_temp[0] == 20.8
-    assert msg.channel_temp[1] != msg.channel_temp[1]
-    assert msg.units == TemperatureUnit.C
+    assert msg.temperatures[0] == 20.8
+    assert msg.temperatures[1] != msg.temperatures[1]
+    assert msg.unit == TemperatureUnit.C
 
 
 def test_decode_ch2():
-    msg = Observation.from_bytes(CH2)
+    msg = Observation.from_bytes(datastrings.CH2[5:5+11])
     assert msg.thermocouple_type == ThermocoupleType.N
-    assert msg.channel_temp[0] != msg.channel_temp[0]
-    assert msg.channel_temp[1] == 20.4
-    assert msg.units == TemperatureUnit.C
+    assert msg.temperatures[0] != msg.temperatures[0]
+    assert msg.temperatures[1] == 20.4
+    assert msg.unit == TemperatureUnit.C
 
 
 def test_decode_kelvin():
-    msg = Observation.from_bytes(BOTH_K)
-    assert msg.channel_temp[0] == BOTH_K_CH1
-    assert msg.channel_temp[1] == BOTH_K_CH2
-    assert msg.units == TemperatureUnit.K
+    msg = Observation.from_bytes(datastrings.BOTH_K[5:5+11])
+    assert msg.temperatures[0] == datastrings.BOTH_K_CH1
+    assert msg.temperatures[1] == datastrings.BOTH_K_CH2
+    assert msg.unit == TemperatureUnit.K
 
 
 def test_decode_reencode():
-    msg = Observation.from_bytes(BOTH)
+    msg = Observation.from_bytes(datastrings.BOTH[5:5+11])
 
     assert msg.thermocouple_type == ThermocoupleType.K
-    assert msg.channel_temp[0] == 22.8
-    assert msg.channel_temp[1] == 23.2
-    assert msg.units == TemperatureUnit.C
-    assert msg.meter_time == timedelta(seconds=30, minutes=28, hours=2)
+    assert msg.temperatures[0] == 22.8
+    assert msg.temperatures[1] == 23.2
+    assert msg.unit == TemperatureUnit.C
+    assert str(msg.meter_time) == "002:28:30"
 
     outb = msg.to_bytes()
-    assert outb == BOTH
+    cmp = datastrings.BOTH[5:5+11]
+    assert outb == cmp
 
 
 def test_convert_synthetic():
-    msg = Observation(channel_temp=[math.nan, math.nan], units='K',
-                      thermocouple_type='N')
+    msg = Observation(temperatures=(math.nan, math.nan),
+                      unit='K', thermocouple_type='N',
+                      meter_time=None, system_time=None)
     out = msg.to_bytes()
     inp = Observation.from_bytes(out)
 
-    assert inp.channel_temp[0] != inp.channel_temp[0]
-    assert inp.channel_temp[1] != inp.channel_temp[1]
+    assert inp.temperatures[0] != inp.temperatures[0]
+    assert inp.temperatures[1] != inp.temperatures[1]
     assert inp.thermocouple_type == ThermocoupleType.N
-    assert inp.units == TemperatureUnit.K
-    assert inp.meter_time == timedelta(seconds=0)
-
-
-def test_framing_empty():
-    (msgs, remb) = Observation.parse_stream(b"")
-    assert len(msgs) == 0
-    assert len(remb) == 0
-
-
-def test_framing_leadingzeros():
-    (msgs, remb) = Observation.parse_stream(FRAMING_LEADZERO)
-    assert len(msgs) == 1
-    assert len(remb) == 0
-    assert msgs[0].channel_temp[0] == BOTH_K_CH1
-    assert msgs[0].channel_temp[1] == BOTH_K_CH2
-
-
-def test_framing_bad_header():
-    (msgs, remb) = Observation.parse_stream(FRAMING_BAD_HEADER)
-    assert len(msgs) == 1
-    assert len(remb) == 0
-    assert msgs[0].channel_temp[0] == BOTH_K_CH1
-    assert msgs[0].channel_temp[1] == BOTH_K_CH2
-
-
-def test_framing_trail_header():
-    (msgs, remb) = Observation.parse_stream(FRAMING_TRAIL_HEADER)
-    assert len(msgs) == 1
-    assert len(remb) == 3
-    assert msgs[0].channel_temp[0] == BOTH_K_CH1
-    assert msgs[0].channel_temp[1] == BOTH_K_CH2
-    assert remb == b"\x65\x14\xFF"
-
-
-def test_framing_two_messages():
-    (msgs, remb) = Observation.parse_stream(FRAMING_TWOMSGS)
-    assert len(msgs) == 2
-    assert len(remb) == 0
-    assert msgs[0].channel_temp[0] == BOTH_K_CH1
-    assert msgs[0].channel_temp[1] == BOTH_K_CH2
-    assert msgs[1].channel_temp[0] == BOTH_K_CH1
-    assert msgs[1].channel_temp[1] == BOTH_K_CH2
+    assert inp.unit == TemperatureUnit.K
+    assert str(inp.meter_time) == "000:00:00"
